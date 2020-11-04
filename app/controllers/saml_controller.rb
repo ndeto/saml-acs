676ee99c86df8b8822ed8f1a1a46da22ac641a4a
ndeto <martindeto@gmail.com>
2020-11-04 15:00:28 +0300

    Setup SSO

---
+++ app/controllers/saml_controller.rb
@@ -0,0 +1,53 @@
+#
+# A SAML service provider controller
+#
+
+class SamlController < ApplicationController
+  # skip_before_action :verify_authenticity_token, :only => [:acs]
+  # skip_before_action :require_authentication
+  # skip_before_action :require_authorization
+
+  #
+  # GET /saml/login
+  #
+  # SP initiated login action. Redirects to IdP.
+  #
+  def login
+    request = OneLogin::RubySaml::Authrequest.new
+    redirect_to(request.create(saml_settings))
+  end
+
+  #
+  # POST /saml/acs
+  #
+  # Assertion Consumer Service URL. The endpoint that the IdP posts to.
+  #
+  def acs
+    response = OneLogin::RubySaml::Response.new(params[:SAMLResponse], :settings => saml_settings)
+    # reset_session
+    session[:user_id] = response.nameid
+    redirect_to start_url
+  end
+
+  #
+  # POST /saml/logout
+  #
+  def logout
+    reset_session
+    redirect_to root_url
+  end
+
+  private
+
+  def saml_settings
+    @settings ||= begin
+                    # if ENV['IDP_METADATA_URL'] && ENV['IDP_METADATA_URL'].present?
+                    #   OneLogin::RubySaml::IdpMetadataParser.new.parse_remote('https://dev-1488456.okta.com/app/exkfyaubp87EmKS885d5/sso/saml/metadata')
+                      OneLogin::RubySaml::IdpMetadataParser.new.parse_remote('https://dev-1488456.okta.com/app/exkdnrv49II4QFLLg5d5/sso/saml/metadata')
+                    # else
+                    #   raise StandardError, "The environment variable IDP_METADATA_URL is not set."
+                    # end
+                  end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'acs/index'
-  get 'acs/consume'
+  get 'saml/login', to: 'saml#login', as: 'login'
+  post 'saml/acs',  to: 'saml#acs'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # post '/acs' => 'acs#'
+  root 'home#index'
 end
